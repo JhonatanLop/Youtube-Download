1ea10d0abe9668cb6c7ac7c79d9246a2babffe2c
JhonatanLop <jhooliveira.lopes@gmail.com>
2022-12-20 19:55:13 -0300

    add escolha de qualidade e op de audio

--- main.py
+++ main.py
@@ -1,21 +1,64 @@
 from pytube import YouTube
 
 # Criando funções com o parâmetro "link"
-def Download(link):
+def DownloadVideo(link):
     video = YouTube(link)
-
-    # escolhe a resolução
-    resolution = input("Qual baixa ou alta resolução? L (baixa)/ H (alta)")
-    # fromata a string pra letra minúscula
-    resolution = resolution.lower()
-    if resolution == "l":
-        video = video.streams.get_lowest_resolution()
-    elif resolution == "h":
-        video = video.streams.get_highest_resolution()
-    else:
-        print("Escolha uma opção válida")
+    try:
+        # escolhe a resolução
+        resolution = input("Qual baixa ou alta resolução? L (baixa)/ H (alta)")
+        # fromata a string pra letra minúscula
+        resolution = resolution.lower()
+        try:
+            if resolution == "l":
+                video = video.streams.get_lowest_resolution()
+                video.download()
+            elif resolution == "h":
+                video = video.streams.get_highest_resolution()
+                video.download()
+            else:
+                print("Escolha uma opção válida")
+                return
+        except:
+            print("Erro, algo aconteceu. Verifique seu link!")
+    except:
+        # caso a entry seja diferente de uma string
+        print("insira um valor válido")
         return
 
-### parte 
+# Donwload apenas de áudio
+def DownloadAudio(link):
+    audio = YouTube(link)
+    try:
+        audio = audio.streams.get_audio_only("mp3")
+        audio.download()
+    except:
+        print("Erro, algo aconteceu. Verifique seu link!")
+        
+### parte de interação com o usuário ###
 link = input("Digite o link aqui:  ")
-Download(link)
+title_video = YouTube(link)
+# print do título do vídeo
+print("\n",title_video.title)
+
+try:
+# joker é o nome que eu dou pra uma variável que o nome não tem importância
+    joker = input("\nEsse é o seu vídeo? s/n    ")
+    joker = joker.lower()
+    if joker == "s":
+        video_or_audio = input("Gostaria de baixar o vídeo ou apenas o Audio? V (vídeo)/A (áudio)   ")
+        video_or_audio = video_or_audio.lower()
+        if video_or_audio == "v":
+            DownloadVideo(link)
+        elif video_or_audio == "a":
+            DownloadAudio(link)
+        else:
+            print("digite uma opção válida")
+    elif joker == "n":
+        print("\nVerifique se o link está correto. Caso o problema persista, contate o suporte\nContato do suporte: jhooliveira.lopes@gmail.com")
+    if video_or_audio == "v":
+        print("Seu vídeo foi baixado!!")
+    else:
+        print("Seu áudio foi baixado!")
+except:
+    print("Insira uma opção válida!")
+
